6917098e0aa9f4ba54241d6e28f40ca07438a544
Marlon <marlonfsolisest@gmail.com>
2022-11-21 21:36:07 -0500

    Adding an util function. Functio name is isKeyValuePair.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,15 @@
+/**
+ * Convert to the type specified.
+ * @param v Value to be converted
+ */
 export const convertTo = function <T>(v: any): T {
     return v as T;
 };
+
+/**
+ * Check if an object is of type key value pair.
+ * @param value Object too be checked.
+ */
+export function isKeyValuePair(value:any) {
+    return value && typeof value === 'object' && value.constructor === Object;
+}
